6f8a672d7ff1baaa0fe8dfd1b9199a8b3e43c0c1
dagbouj hatem <dagboujhatem@gmail.com>
2022-12-08 17:59:17 +0100

    add comment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
-// 
+
+// import sass global file
 import './styles/global.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
